d87419968c2cdd6f21e3603e1a67b52544f80c65
volkov <imwolkow@gmail.com>
2022-11-08 22:43:12 +0300

    save logs to file on disk

--- start.sh
+++ start.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 JAR_NAME=wiremock-jre8-standalone-2.35.0.jar
+LOG_NAME=log.txt
 PORT=8081
-THREADS=1000
+THREADS=100
 
 java -jar $JAR_NAME \
 	--port $PORT \
-	--verbose \
 	--no-request-journal \
 	--async-response-enabled \
 	--async-response-threads $THREADS \
-	
+	--disable-banner \
+	--verbose \
+  | tee $LOG_NAME
