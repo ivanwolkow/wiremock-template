f3db3122b409654af4fa014b6e8706f3e3e7d85f
volkov <imwolkow@gmail.com>
2022-11-15 18:26:54 +0300

    fix wiremock argument async-response-enabled

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@
 JAR_NAME=wiremock-jre8-standalone-2.35.0.jar
 LOG_NAME=log.txt
 PORT=8081
-THREADS=100
+THREADS=10
 
 java -jar $JAR_NAME \
 	--port $PORT \
 	--no-request-journal \
-	--async-response-enabled \
+	--async-response-enabled true \
 	--async-response-threads $THREADS \
 	--disable-banner \
 	--verbose \
